07ea936342d9ddcd754acc9785622b18a7cbc5dc
NanoRocky <3525987739@qq.com>
2025-03-09 16:44:21 +0800

    Custom

--- src/views/MoreSet/index.vue
+++ src/views/MoreSet/index.vue
@@ -1,14 +1,42 @@
 <template>
-  <div class="set" @mouseenter="closeShow = true" @mouseleave="closeShow = false" @click.stop>
+
+  <div class="mobileset" v-if="store.mobileOpenState" @mouseenter="closeShow = true" @mouseleave="closeShow = false"
+    @click.stop>
     <transition name="el-fade-in-linear">
-      <close-one
-        class="close"
-        theme="filled"
-        size="28"
-        fill="#ffffff60"
-        v-show="closeShow"
-        @click="store.setOpenState = false"
-      />
+      <close-one class="close" theme="filled" size="28" fill="#ffffff60" v-show="closeShow"
+        @click="store.setOpenState = false" />
+    </transition>
+    <el-row>
+      <el-col class="left">
+        <div class="logo text-hidden">
+          <span class="bg">{{ siteUrl[0] }}</span>
+          <span class="sm">.{{ siteUrl[1] }}</span>
+        </div>
+        <!-- 移动端设置菜单 -->
+        <div class="title">
+          <setting-two theme="filled" size="28" fill="#ffffff60" />
+          <span class="name">全局设置</span>
+        </div>
+        <div class="mobileset-scrollable">
+          <Set />
+        </div>
+        <div class="version">
+          <div class="num">v&nbsp;{{ config.version }}</div>
+          <el-tooltip content="Powered by imsyy" placement="right" :show-arrow="false">
+            <github-one class="github" theme="outline" size="24" @click="jumpTo(config.github)" />
+          </el-tooltip>
+          <el-tooltip content="Extension Function Updates by NanoRocky" placement="right" :show-arrow="false">
+            <file-editing-one class="github" theme="outline" size="24" @click="jumpTo(config.efug)" />
+          </el-tooltip>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+
+  <div class="set" v-else @mouseenter="closeShow = true" @mouseleave="closeShow = false" @click.stop>
+    <transition name="el-fade-in-linear">
+      <close-one class="close" theme="filled" size="28" fill="#ffffff60" v-show="closeShow"
+        @click="store.setOpenState = false" />
     </transition>
     <el-row :gutter="40">
       <el-col :span="12" class="left">
@@ -21,21 +49,27 @@
           <el-tooltip content="Powered by imsyy" placement="right" :show-arrow="false">
             <github-one class="github" theme="outline" size="24" @click="jumpTo(config.github)" />
           </el-tooltip>
+          <el-tooltip content="Extension Function Updates by NanoRocky" placement="right" :show-arrow="false">
+            <file-editing-one class="github" theme="outline" size="24" @click="jumpTo(config.efug)" />
+          </el-tooltip>
         </div>
       </el-col>
+      <!-- 桌面端设置菜单 -->
       <el-col :span="12" class="right">
         <div class="title">
           <setting-two theme="filled" size="28" fill="#ffffff60" />
           <span class="name">全局设置</span>
         </div>
-        <Set />
+        <div class="set-scrollable">
+          <Set />
+        </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script setup>
-import { CloseOne, SettingTwo, GithubOne, AddOne, Bug } from "@icon-park/vue-next";
+import { CloseOne, SettingTwo, GithubOne, AddOne, Bug, FileEditingOne } from "@icon-park/vue-next";
 import { mainStore } from "@/store";
 import Set from "@/components/Set.vue";
 import config from "@/../package.json";
@@ -109,6 +143,7 @@ const jumpTo = (url) => {
         width: 100%;
         height: 260px;
         min-height: 140px;
+
         .bg {
           font-size: 5rem;
         }
@@ -122,14 +157,17 @@ const jumpTo = (url) => {
           .bg {
             font-size: 4.5rem;
           }
+
           .sm {
             font-size: 1.7rem;
           }
         }
+
         @media (max-width: 825px) {
           .bg {
             font-size: 3.8rem;
           }
+
           .sm {
             font-size: 1.3rem;
           }
@@ -142,7 +180,7 @@ const jumpTo = (url) => {
         align-items: center;
 
         .num {
-          font-size: 2rem;
+          font-size: 1rem;
           font-family: "字魂萌趣软糖体";
         }
 
@@ -209,6 +247,146 @@ const jumpTo = (url) => {
           width: 28px;
           height: 28px;
           margin-right: 6px;
+        }
+      }
+
+      .set-scrollable {
+        flex: 1;
+        overflow-y: auto;
+      }
+    }
+  }
+}
+
+.mobileset {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  width: 82%;
+  height: 86%;
+  background: rgb(255 255 255 / 40%);
+  border-radius: 6px;
+  padding: 36px;
+
+  .close {
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    width: 28px;
+    height: 28px;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+
+    &:active {
+      transform: scale(1);
+    }
+  }
+
+  .el-row {
+    height: 100%;
+    flex-wrap: nowrap;
+
+    .left {
+      height: 100%;
+      padding-left: 24px !important;
+      padding-bottom: 24px;
+      display: flex;
+      flex-direction: column;
+      // justify-content: space-between;
+
+      .logo {
+        transform: translateY(-8%);
+        font-family: "字魂元气酪酪体";
+        padding-left: 6px;
+        width: 72%;
+        height: auto;
+
+        .bg {
+          font-size: 2rem;
+        }
+
+        .sm {
+          margin-left: 6px;
+          font-size: 1.2rem;
+        }
+      }
+
+      .title {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        font-size: 18px;
+        margin-bottom: 16px;
+        padding-top: 18px;
+
+        .i-icon {
+          width: 28px;
+          height: 28px;
+          margin-right: 6px;
+        }
+      }
+
+      .mobileset-scrollable {
+        flex: 1;
+        overflow-y: auto;
+      }
+
+      .version {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-top: auto;
+
+        .num {
+          font-size: 1rem;
+          font-family: "字魂萌趣软糖体";
+        }
+
+        .github {
+          width: 24px;
+          height: 24px;
+          margin-left: 12px;
+          margin-top: 6px;
+
+          &:hover {
+            transform: scale(1.2);
+          }
+        }
+      }
+
+      .update {
+        margin-top: 30px;
+        height: 100%;
+
+        :deep(.el-card__body) {
+          height: 100%;
+
+          .upnote {
+            padding: 20px;
+            height: calc(100% - 56px);
+            overflow-y: auto;
+
+            .uptext {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              padding-bottom: 16px;
+
+              &:nth-last-of-type(1) {
+                padding: 0;
+              }
+
+              .i-icon {
+                width: 22px;
+                height: 22px;
+                margin-right: 8px;
+              }
+            }
+          }
         }
       }
     }
