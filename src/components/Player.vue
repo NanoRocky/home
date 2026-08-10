0b89ad678cb9956ec49a38c3bd022b122cbfa83d
NanoRocky <76585834+NanoRocky@users.noreply.github.com>
2026-08-10 15:43:48 +0800

    [UPD]更新 amll-ttml-db 仓库 URL

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -417,12 +417,12 @@ const fetchDWRC = async (dwrcUrl: string, expectedIndex: number) => {
   if (store.playerDWRCATDB) {
     const songUrlInfUrl = store.playerDWRCATDBF
       ? {
-        netease: `https://ghfast.top/https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
-        tencent: `https://ghfast.top/https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
+        netease: `https://ghfast.top/https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
+        tencent: `https://ghfast.top/https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
       }
       : {
-        netease: `https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
-        tencent: `https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
+        netease: `https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
+        tencent: `https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
       };
     if (!songServer || !["netease", "tencent"].includes(songServer)) {
       return;
@@ -450,8 +450,8 @@ const fetchDWRC = async (dwrcUrl: string, expectedIndex: number) => {
     } catch (e) {
       if (store.playerDWRCATDBF) {
         const songUrlInfUrlse = {
-          netease: `https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
-          tencent: `https://raw.githubusercontent.com/Steve-xmh/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
+          netease: `https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/ncm-lyrics/${songId}.yrc`,
+          tencent: `https://raw.githubusercontent.com/amll-dev/amll-ttml-db/main/qq-lyrics/${songId}.qrc`,
         };
         if (songServer || ["netease", "tencent"].includes(songServer)) {
           try {
